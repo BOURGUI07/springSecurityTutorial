08118493a0cf51b51653aaeb450ce728e3792a04
Youness Bourgui <younessbourgui07@gmail.com>
2024-07-29 16:44:59 +0200

    Customize and Implement Our Own Auth Provider

--- src/main/java/com/example/securityTutorial/config/SecurityConfig.java
+++ src/main/java/com/example/securityTutorial/config/SecurityConfig.java
@@ -4,20 +4,14 @@
  */
 package com.example.securityTutorial.config;
 
-import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import static org.springframework.security.config.Customizer.withDefaults;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
 
---
+++ src/main/java/com/example/securityTutorial/service/CustomerUsernamePwdAuthProvider.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.securityTutorial.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author hp
+ */
+@Component
+@RequiredArgsConstructor
+public class CustomerUsernamePwdAuthProvider implements AuthenticationProvider {
+    
+    private final UserDetailsService service;
+    private final PasswordEncoder encoder;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        var username = authentication.getName();
+        var password = authentication.getCredentials().toString();
+        var user = service.loadUserByUsername(username);
+        if(encoder.matches(password, user.getPassword())){
+            return new UsernamePasswordAuthenticationToken(username,password,user.getAuthorities());
+        }else{
+            throw new BadCredentialsException("Invalid Password!");
+        }
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return (UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication));
+    }
+    
+}
